52e9b4246989c1f7c18fb6570d6e84fee96f27df
Willium Tiwari <williumtiwari123@gmail.com>
2024-05-10 16:09:21 +0530

    Services Updated

--- src/app/services/github-api.service.spec.ts
+++ src/app/services/github-api.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import { GithubApiService } from './github-api.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe('GithubApiService', () => {
+  let service: GithubApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    service = TestBed.inject(GithubApiService);
   });
 
   it('should be created', () => {
